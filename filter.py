f3f914f7a16adc5ee39ad7796e6d792fe6d5e5fd
Dmitry Ishoev <26188825+NetVoobrazhenia@users.noreply.github.com>
2021-11-10 17:42:02 +0500

    Completed the 1st stage

--- filter.py
+++ filter.py
@@ -1,27 +1,27 @@
-from PIL import Image
 import numpy as np
+from PIL import Image
+
 img = Image.open("img2.jpg")
 arr = np.array(img)
 a = len(arr)
 a1 = len(arr[1])
 i = 0
-while i < a - 11:
+while i < a:
     j = 0
-    while j < a1 - 11:
+    while j < a1:
         s = 0
         for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
-        s = int(s // 100)
+            for n0 in range(j, j + 10):
+                n1 = int(arr[n][n0][0])
+                n2 = int(arr[n][n0][1])
+                n3 = int(arr[n][n0][2])
+                s += (n1 + n2 + n3)/3
+        s = s // 100
         for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
+            for n0 in range(j, j + 10):
+                arr[n][n0][0] = int(s // 50) * 50
+                arr[n][n0][1] = int(s // 50) * 50
+                arr[n][n0][2] = int(s // 50) * 50
         j = j + 10
     i = i + 10
 res = Image.fromarray(arr)
