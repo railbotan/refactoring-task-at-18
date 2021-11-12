65b0c3a844338ecd64dbd3f62adb70c09df9f25e
Sergey <seregapax@gmail.com>
2021-11-13 01:16:09 +0500

    Make stage 4.

--- filter.py
+++ filter.py
@@ -1,6 +1,10 @@
 import os
+import argparse
 import numpy as np
 from PIL import Image, UnidentifiedImageError
+
+STANDARD_PIXEL_SIZE = 10
+STANDARD_GRAYSCALE = 50
 
 
 def load_img_as_array(path: str) -> np.ndarray:
@@ -9,11 +13,14 @@ def load_img_as_array(path: str) -> np.ndarray:
             img = Image.open(path)
             return np.array(img)
         except UnidentifiedImageError:
-            raise TypeError("Incorrect file type.")
-    raise FileExistsError("This file doesn't exist.")
+            print("Incorrect file type.")
+            exit(1)
+    print("This file doesn't exist.")
+    exit(1)
 
 
-def convert_to_gray_pixel_art(img: np.ndarray, pixel_size: int = 10, grayscale: int = 50) -> np.ndarray:
+def convert_to_gray_pixel_art(img: np.ndarray, pixel_size: int = STANDARD_PIXEL_SIZE,
+                              grayscale: int = STANDARD_GRAYSCALE) -> np.ndarray:
     for x in range(0, len(img), pixel_size):
         for y in range(0, len(img[0]), pixel_size):
             brightness = np.average(img[x: x + pixel_size, y: y + pixel_size])
@@ -26,10 +33,27 @@ def save_img(img: np.ndarray, filename: str) -> None:
     Image.fromarray(img).save(filename)
 
 
+def convert(input_path: str, output_path: str = None, pixel_size: int = STANDARD_PIXEL_SIZE,
+            grayscale: int = STANDARD_GRAYSCALE) -> None:
+    img = load_img_as_array(input_path)
+    gray_image = convert_to_gray_pixel_art(img, pixel_size, grayscale)
+    file_info = os.path.splitext(input_path)
+    output_path = output_path or f"{file_info[0]}_pixel{file_info[1]}"
+    save_img(gray_image, output_path)
+
+
 def main() -> None:
-    img = load_img_as_array("img2.jpg")
-    gray_image = convert_to_gray_pixel_art(img, 10, 50)
-    save_img(gray_image, "res.jpg")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_path', type=str, help='Путь до входного изображения.')
+    parser.add_argument('-op', '--output_path', type=str, default=None,
+                        help=('Путь до выходного изображения. Стандартное значение: '
+                              '<путь до файла>_pixel.<расширение файла>.'))
+    parser.add_argument('-ps', '--pixel_size', type=int, default=10,
+                        help=f'Устанавливает размер пикселя. Стандартное значение: {STANDARD_PIXEL_SIZE}.')
+    parser.add_argument('-gs', '--grayscale', type=int, default=50,
+                        help=f'Устанавливает градацию серого. Стандартное значение: {STANDARD_GRAYSCALE}.')
+    args = parser.parse_args()
+    convert(args.input_path, args.output_path, args.pixel_size, args.grayscale)
 
 
 if __name__ == "__main__":
