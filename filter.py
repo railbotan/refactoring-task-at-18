c5dd3459dc41850792bfcaf035b0466c8651954d
Sergey <seregapax@gmail.com>
2021-11-13 00:29:13 +0500

    Make stage 3.

--- filter.py
+++ filter.py
@@ -14,20 +14,10 @@ def load_img_as_array(path: str) -> np.ndarray:
 
 
 def convert_to_gray_pixel_art(img: np.ndarray, pixel_size: int = 10, grayscale: int = 50) -> np.ndarray:
-    width = len(img)
-    height = len(img[1])
-
-    for x in range(0, width, pixel_size):
-        for y in range(0, height, pixel_size):
-            brightness = 0
-            for x1 in range(x, min(x + pixel_size, width)):
-                for y1 in range(y, min(y + pixel_size, height)):
-                    brightness += sum(int(color) for color in img[x1][y1]) // 3
-            brightness = brightness // (pixel_size * pixel_size)
-
-            for x1 in range(x, min(x + pixel_size, width)):
-                for y1 in range(y, min(y + pixel_size, height)):
-                    img[x1][y1][0] = img[x1][y1][1] = img[x1][y1][2] = brightness - brightness % grayscale
+    for x in range(0, len(img), pixel_size):
+        for y in range(0, len(img[0]), pixel_size):
+            brightness = np.average(img[x: x + pixel_size, y: y + pixel_size])
+            img[x: x + pixel_size, y: y + pixel_size] = brightness - brightness % grayscale
 
     return img
 
