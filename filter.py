164ee6802e7002fde10c22c7c3e3288575c0406c
Sergey <seregapax@gmail.com>
2021-11-12 23:42:00 +0500

    Make stage 2.

--- filter.py
+++ filter.py
@@ -1,29 +1,46 @@
+import os
 import numpy as np
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                m1 = int(arr[n][n1][0])
-                m2 = int(arr[n][n1][1])
-                m3 = int(arr[n][n1][2])
-                M = m1 + m2 + m3
-                s += M // 3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+def load_img_as_array(path: str) -> np.ndarray:
+    if os.path.isfile(path):
+        try:
+            img = Image.open(path)
+            return np.array(img)
+        except UnidentifiedImageError:
+            raise TypeError("Incorrect file type.")
+    raise FileExistsError("This file doesn't exist.")
+
+
+def convert_to_gray_pixel_art(img: np.ndarray, pixel_size: int = 10, grayscale: int = 50) -> np.ndarray:
+    width = len(img)
+    height = len(img[1])
+
+    for x in range(0, width, pixel_size):
+        for y in range(0, height, pixel_size):
+            brightness = 0
+            for x1 in range(x, min(x + pixel_size, width)):
+                for y1 in range(y, min(y + pixel_size, height)):
+                    brightness += sum(int(color) for color in img[x1][y1]) // 3
+            brightness = brightness // (pixel_size * pixel_size)
+
+            for x1 in range(x, min(x + pixel_size, width)):
+                for y1 in range(y, min(y + pixel_size, height)):
+                    img[x1][y1][0] = img[x1][y1][1] = img[x1][y1][2] = brightness - brightness % grayscale
+
+    return img
+
+
+def save_img(img: np.ndarray, filename: str) -> None:
+    Image.fromarray(img).save(filename)
+
+
+def main() -> None:
+    img = load_img_as_array("img2.jpg")
+    gray_image = convert_to_gray_pixel_art(img, 10, 50)
+    save_img(gray_image, "res.jpg")
+
+
+if __name__ == "__main__":
+    main()
