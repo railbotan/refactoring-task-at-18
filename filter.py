a17eec3b00b3a8f0849e507fff5f070d2674c78b
AnemonOFF <dimonworld7778@gmail.com>
2021-11-13 17:37:47 +0500

    Complete 3rd stage

--- filter.py
+++ filter.py
@@ -13,30 +13,17 @@ def save_image(img: np.ndarray, name: str) -> None:
 
 
 def get_brightness(x: int, y: int, img: np.ndarray, pixel_height: int, pixel_width: int, gray_step: int):
-    res = 0
-    for height in range(y, y + pixel_height):
-        for width in range(x, x + pixel_width):
-            channel_1 = int(img[height][width][0])
-            channel_2 = int(img[height][width][1])
-            channel_3 = int(img[height][width][2])
-            res += (channel_1 + channel_2 + channel_3) / 3
-    res = res // (pixel_height * pixel_width)
+    res = np.average(img[y: y + pixel_height, x: x + pixel_width])
     return res - res % gray_step
 
 
 def create_gray_mosaic(img: np.ndarray, pixel_height: int, pixel_width: int, gray_step: int) -> np.ndarray:
     res_img = img.copy()
-    img_height = len(img)
-    img_width = len(img[1])
 
-    for y in range(0, img_height, pixel_height):
-        for x in range(0, img_width, pixel_width):
+    for y in range(0, len(img), pixel_height):
+        for x in range(0, len(img[0]), pixel_width):
             brightness = get_brightness(x, y, img, pixel_height, pixel_width, gray_step)
-            for height in range(y, y + pixel_height):
-                for width in range(x, x + pixel_width):
-                    res_img[height][width][0] = brightness
-                    res_img[height][width][1] = brightness
-                    res_img[height][width][2] = brightness
+            res_img[y: y + pixel_height, x: x + pixel_width] = brightness
 
     return res_img
 
