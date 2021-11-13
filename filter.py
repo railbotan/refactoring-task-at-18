309e5c0b6d048ee9ebfca1afea16dd914ead9bcf
speedUpDev <70540091+speedUpDev@users.noreply.github.com>
2021-11-13 21:19:52 +0500

    Fourth commit
    Выполнен этап 4

--- filter.py
+++ filter.py
@@ -13,21 +13,27 @@ def set_grey_pixels(colorSum, i, j):
     arr[i:i + pxSize, j:j + pxSize, 2] = int(colorSum // gradation) * gradation
 
 
-img = Image.open("img2.jpg")
+def img2pixel(original, result):
+    global arr
+    global gradation
+    global pxSize
+    img = Image.open(original)
+    arr = np.array(img)
+    height = len(arr)
+    width = len(arr[1])
+    i = 0
 
-arr = np.array(img)
-height = len(arr)
-width = len(arr[1])
-i = 0
+    gradation = int(input('Введите градацию серого: '))
+    pxSize = int(input('Введите размер мазайки: '))
+    while i < height:
+        j = 0
+        while j < width:
+            s = get_pixel(i, j)
+            set_grey_pixels(s, i, j)
+            j = j + pxSize
+        i = i + pxSize
+    res = Image.fromarray(arr)
+    res.save(result)
 
-gradation = int(input('Введите градацию серого: '))
-pxSize = int(input('Введите размер мазайки: '))
-while i < height:
-    j = 0
-    while j < width:
-        s = get_pixel(i, j)
-        set_grey_pixels(s, i, j)
-        j = j + pxSize
-    i = i + pxSize
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+img2pixel(input("Название оригинала: "), input("Название копии: "))
