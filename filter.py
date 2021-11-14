fe35f62264d37d81c8ec84fec8c3c86531b188a0
ze-yter <zeyterforurfu@gmail.com>
2021-11-14 16:04:06 +0500

    Stage 2 - highlighting functions

--- filter.py
+++ filter.py
@@ -1,31 +1,47 @@
 from PIL import Image
 import numpy as np
 
+
+def MosaicSum(array, size, i, j):
+    sum = 0
+    
+    for x in range(i, i + size):
+        for y in range(j, j + size):
+            redGrad = array[x][y][0]
+            greenGrad = array[x][y][1]
+            blueGrad = array[x][y][2]
+            M = redGrad + greenGrad + blueGrad
+            sum += M
+    
+    return sum
+
+
+def MakeColor(newMatrix, matrix, size, i, j):
+    for x in range(i, i + size):
+        for y in range(j, j + size):
+            for z in range(3):
+                matrix[x][y][z] = newMatrix
+
+
+def CreateMosaicIMG(img, size, grad):
+    limit = 255 // grad
+    listImg = np.array(img).astype(int)
+    lenImg = len(listImg)
+    height = len(listImg[0])
+    i = 0
+    
+    while i < lenImg:
+        j = 0
+        while j < height:
+            sum = MosaicSum(listImg, size, i, j)
+            avg = int(sum // (size ** 2))
+            MakeColor(int(avg // limit) * limit / 3, listImg, size, i, j)
+            j += size
+        i += size
+    
+    return Image.fromarray(np.uint8(listImg))
+
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                m1 = arr[n][n1][0]
-                m2 = arr[n][n1][1]
-                m3 = arr[n][n1][2]
-                M = m1 + m2 + m3
-                s += M // 3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-
-res = Image.fromarray(arr)
-res.save('res.jpg')
+resolution = CreateMosaicIMG(img, size = 15, grad = 5)
+resolution.save('res.jpg')
