572e23938e6ba1dea550c051f031e85c3d705feb
Igor <roggi.7@mail.ru>
2021-11-14 21:32:11 +0500

    stage 2

--- filter.py
+++ filter.py
@@ -1,28 +1,37 @@
 from PIL import Image
 import numpy as np
+
+def Get_gray_sum (arr, i, j, slice):
+    gray_sum = 0
+    for x in range(i, i + slice):
+        for y in range(j, j + slice):
+            red = arr[x][y][0]
+            green = arr[x][y][1]
+            blue = arr[x][y][2]
+            middle_pixel = (int(red) + int(green) + int(blue)) / 3
+            gray_sum += middle_pixel
+    gray_sum = int(gray_sum // slice**2)
+    return gray_sum
+
+def Create_color (arr, i, j, gray_sum, slice, step):
+    for x in range(i, i + slice):
+        for y in range(j, j + slice):
+            for z in range (3):
+                arr[x][y][z] = int(gray_sum // step) * step
+
 img = Image.open("img2.jpg")
 arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
+pixels_count = len(arr)
+width = len(arr[1])
 i = 0
-while i < a - 9:
+while i < pixels_count - 9:
     j = 0
-    while j < a1 - 9:
-        s = 0
-        for n in range(i, i + 10):
-            for q in range(j, j + 10):
-                n1 = arr[n][q][0]
-                n2 = arr[n][q][1]
-                n3 = arr[n][q][2]
-                M = (int(n1) + int(n2) + int(n3)) / 3
-                s += M
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
+    while j < width - 9:
+        gray_sum = Get_gray_sum (arr, i, j, 10)
+        Create_color(arr, i, j, gray_sum, 10, 50)
+        j += 10
+    i += 10
 res = Image.fromarray(arr)
 res.save('res.jpg')
+
+
