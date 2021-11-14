b8535886d9d0f77dc973ea37955009be148c1aa7
GUIDE-WS <IgorGrigorev04@gmail.com>
2021-11-14 14:32:40 +0500

    Task 2

--- filter.py
+++ filter.py
@@ -1,29 +1,39 @@
 import numpy as np
 from PIL import Image
 
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                n1 = int(arr[n][m][0])
-                n2 = int(arr[n][m][1])
-                n3 = int(arr[n][m][2])
-                M = n1 + n2 + n3
-                s += M // 3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                arr[n][m][0] = int(s // 50) * 50
-                arr[n][m][1] = int(s // 50) * 50
-                arr[n][m][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+
+def img_to_nparr(path):
+    img = Image.open(path)
+    return np.array(img)
+
+
+def save_img_from_nparr(img, name):
+    res = Image.fromarray(img)
+    res.save(name)
+
+
+def create_pixel_art(img, size, grayscale):
+    width = len(img)
+    height = len(img[1])
+
+    for x in range(0, width, size):
+        for y in range(0, height, size):
+            brightness = get_average_brightness(height, img, size, width, x, y)
+            for x1 in range(x, min(x + size, width)):
+                for y1 in range(y, min(y + size, height)):
+                    img[x1][y1][0] = img[x1][y1][1] = img[x1][y1][2] = brightness - brightness % grayscale
+
+    return img
+
+
+def get_average_brightness(height, img, size, width, x, y):
+    brightness = 0
+    for i in range(x, min(x + size, width)):
+        for j in range(y, min(y + size, height)):
+            brightness += sum(int(color) for color in img[i][j]) // 3
+    brightness = brightness // (size * size)
+    return brightness
+
+
+img = img_to_nparr("img2.jpg")
+save_img_from_nparr(create_pixel_art(img, 10, 50), 'res.jpg')
