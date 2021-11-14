f9ee6c7d2e87e3e84da7f142486b3474556272bd
Igor <roggi.7@mail.ru>
2021-11-14 22:49:34 +0500

    stage 2 - edit

--- filter.py
+++ filter.py
@@ -1,37 +1,33 @@
 from PIL import Image
 import numpy as np
 
-def Get_gray_sum (arr, i, j, slice):
+def Get_gray_sum (img_list, i, j, area):
     gray_sum = 0
-    for x in range(i, i + slice):
-        for y in range(j, j + slice):
-            red = arr[x][y][0]
-            green = arr[x][y][1]
-            blue = arr[x][y][2]
+    for x in range(i, i + area):
+        for y in range(j, j + area):
+            red = img_list[x][y][0]
+            green = img_list[x][y][1]
+            blue = img_list[x][y][2]
             middle_pixel = (int(red) + int(green) + int(blue)) / 3
             gray_sum += middle_pixel
-    gray_sum = int(gray_sum // slice**2)
+    gray_sum = int(gray_sum // area**2)
     return gray_sum
 
-def Create_color (arr, i, j, gray_sum, slice, step):
-    for x in range(i, i + slice):
-        for y in range(j, j + slice):
+def Create_color (img_list, i, j, gray_sum, area, step):
+    for x in range(i, i + area):
+        for y in range(j, j + area):
             for z in range (3):
-                arr[x][y][z] = int(gray_sum // step) * step
+                img_list[x][y][z] = int(gray_sum // step) * step
 
-img = Image.open("img2.jpg")
-arr = np.array(img)
-pixels_count = len(arr)
-width = len(arr[1])
+img_list = np.array(Image.open("img2.jpg"))
+pixels_count = len(img_list)
+width = len(img_list[1])
 i = 0
 while i < pixels_count - 9:
     j = 0
     while j < width - 9:
-        gray_sum = Get_gray_sum (arr, i, j, 10)
-        Create_color(arr, i, j, gray_sum, 10, 50)
+        gray_sum = Get_gray_sum (img_list, i, j, 10)
+        Create_color(img_list, i, j, gray_sum, 10, 6)
         j += 10
     i += 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
-
-
+Image.fromarray(img_list).save('res.jpg')
