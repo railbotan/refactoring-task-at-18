0c6c97f72fd14164c46fea73602a2adf427af8e9
DSadrium <argamak-94@mail.ru>
2021-11-14 14:41:43 +0500

    Task №2

--- filter.py
+++ filter.py
@@ -1,28 +1,39 @@
-from PIL import Image
 import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                n1 = int(arr[n][m][0])
-                n2 = int(arr[n][m][1])
-                n3 = int(arr[n][m][2])
-                M = n1 + n2 + n3
-                s += M // 3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for m in range(j, j + 10):
-                arr[n][m][0] = int(s // 50) * 50
-                arr[n][m][1] = int(s // 50) * 50
-                arr[n][m][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
-res.save('res.jpg')
+from PIL import Image
+
+
+def get_img(path):
+    img = Image.open(path)
+    return np.array(img)
+
+
+def save_img(img, name):
+    res = Image.fromarray(img)
+    res.save(name)
+
+
+def get_pixel_art(img, size, grayscale):
+    width = len(img)
+    height = len(img[1])
+
+    for x in range(0, width, size):
+        for y in range(0, height, size):
+            brightness = get_brightness(height, img, size, width, x, y)
+            for i in range(x, min(x + size, width)):
+                for j in range(y, min(y + size, height)):
+                    img[i][j][0] = img[i][j][1] = img[i][j][2] = brightness - brightness % grayscale
+
+    return img
+
+
+def get_brightness(height, img, size, width, x, y):
+    brightness = 0
+    for i in range(x, min(x + size, width)):
+        for j in range(y, min(y + size, height)):
+            brightness += sum(int(color) for color in img[i][j]) // 3
+    brightness = brightness // (size * size)
+    return brightness
+
+
+img = get_img("img2.jpg")
+save_img(get_pixel_art(img, 10, 50), 'res.jpg')
