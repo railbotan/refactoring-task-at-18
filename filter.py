96fbc2eeb4c465cdf77162ad83e068bfd1b6eb31
alyona_filyaeva <alyonchikfilyaeva@yandex.ru>
2021-11-16 10:05:10 +0500

    Task 3

--- filter.py
+++ filter.py
@@ -4,24 +4,14 @@ img = Image.open(input())
 pixels = np.array(img)
 
 def get_gray(pix_width, pix_height, x, y):
-    result = 0
-    for width in range(pix_width, pix_width + x):
-        for height in range(pix_height, pix_height + y):
-            color_1 = int(pixels[width][height][0])
-            color_2 = int(pixels[width][height][1])
-            color_3 = int(pixels[width][height][2])
-            result += (color_1 + color_2 + color_3) / 3
+    result = np.sum(pixels[pix_width: pix_width + x,pix_height: pix_height + y ]) / 3
     result = int(result // (x * y))
     return result
 
 
 def replace_pixels(pix_width, x, pix_height, y, step_gray):
     gray = get_gray(pix_width, pix_height, x, y)
-    for width in range(pix_width, pix_width + x):
-        for height in range(pix_height, pix_height + y):
-            pixels[width][height][0] = int(gray // step_gray) * step_gray
-            pixels[width][height][1] = int(gray // step_gray) * step_gray
-            pixels[width][height][2] = int(gray // step_gray) * step_gray
+    pixels[pix_width: pix_width + x,pix_height: pix_height + y ] = int(gray // step_gray) * step_gray
 
 
 def get_gray_img(x, y, step_gray):
