a0422536cd45b3afdbd0472001486aa636203941
NastyaBay <nastya23700@mail.ru>
2021-11-17 22:40:35 +0500

    этап-4(переделанный)

--- filter.py
+++ filter.py
@@ -2,17 +2,17 @@ from PIL import Image
 import numpy as np
 
 
-def count_gray(pixels, pixels_x, pixels_y, size_x, size_y):
-    gray = np.sum((pixels[pixels_x: pixels_x + size_x, pixels_y: pixels_y + size_y]) / 3)
-    return int(gray // (size_x * size_y))
+def count_gray(pixels, pixels_x, pixels_y, size):
+    gray = np.sum((pixels[pixels_x: pixels_x + size, pixels_y: pixels_y + size]) / 3)
+    return int(gray // (size * size))
 
 
-def replace_pixels(pixels, pixels_x, size_x, pixels_y, size_y, step):
-    gray = count_gray(pixels, pixels_x, pixels_y, size_x, size_y)
-    pixels[pixels_x: pixels_x + size_x, pixels_y: pixels_y + size_y] = int(gray // step) * step
+def replace_pixels(pixels, pixels_x, pixels_y, size, step):
+    gray = count_gray(pixels, pixels_x, pixels_y, size)
+    pixels[pixels_x: pixels_x + size, pixels_y: pixels_y + size] = int(gray // step) * step
 
 
-def convert_gray_img(pixels, size_x, size_y, grey_step):
+def convert_gray_img(pixels, size, grey_step):
     step = 255 // (grey_step - 1)
     height = len(pixels)
     width = len(pixels[1])
@@ -20,13 +20,16 @@ def convert_gray_img(pixels, size_x, size_y, grey_step):
     while pixels_x < height:
         pixels_y = 0
         while pixels_y < width:
-            replace_pixels(pixels, pixels_x, size_x, pixels_y, size_y, step)
-            pixels_y = pixels_y + size_y
-        pixels_x = pixels_x + size_x
+            replace_pixels(pixels, pixels_x, pixels_y, size, step)
+            pixels_y = pixels_y + size
+        pixels_x = pixels_x + size
     return pixels
 
 
-img = Image.open("img2.jpg")
+img = Image.open(input('Имя входного файла:'))
 pixels_img = np.array(img)
-res = Image.fromarray(convert_gray_img(pixels_img, 10, 10, 5))
-res.save('res.jpg')
+size_pixel = int(input('Размер пикселя(блока):'))
+step_gray = int(input('Количество градаций:'))
+res_file = input('Имя для сохранения файла:')
+res = Image.fromarray(convert_gray_img(pixels_img, size_pixel, step_gray))
+res.save(res_file)
