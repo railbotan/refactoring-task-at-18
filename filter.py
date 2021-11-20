c8036c46a60950dbd080e44ff9af3274e446451b
AnemonOFF <dimonworld7778@gmail.com>
2021-11-21 00:13:14 +0500

    Review fix

--- filter.py
+++ filter.py
@@ -12,25 +12,33 @@ def save_image(img: np.ndarray, name: str) -> None:
     res.save(name)
 
 
-def get_brightness(x: int, y: int, img: np.ndarray, pixel_height: int, pixel_width: int, gray_step: int):
-    res = np.average(img[y: y + pixel_height, x: x + pixel_width])
+def get_brightness(x: int, y: int, img: np.ndarray, block_height: int, block_width: int, gray_step: int):
+    res = np.average(img[y: y + block_height, x: x + block_width])
     return res - res % gray_step
 
 
-def create_gray_mosaic(img: np.ndarray, pixel_height: int, pixel_width: int, gray_step: int) -> np.ndarray:
+def create_gray_mosaic(img: np.ndarray, block_height: int, block_width: int, gray_step: int) -> np.ndarray:
+    print("In progress... Please, wait.")
     res_img = img.copy()
 
-    for y in range(0, len(img), pixel_height):
-        for x in range(0, len(img[0]), pixel_width):
-            brightness = get_brightness(x, y, img, pixel_height, pixel_width, gray_step)
-            res_img[y: y + pixel_height, x: x + pixel_width] = brightness
+    for y in range(0, len(img), block_height):
+        for x in range(0, len(img[0]), block_width):
+            brightness = get_brightness(x, y, img, block_height, block_width, gray_step)
+            res_img[y: y + block_height, x: x + block_width] = brightness
 
     return res_img
 
 
+def get_step(gray_scale: int) -> int:
+    if gray_scale < 1 or gray_scale > 255:
+        raise ValueError(f"Number of gradations must be in [1, 255]. You input: {gray_scale}")
+    gray_scale -= 1
+    return int(255 / gray_scale - 1 if 255 % gray_scale == 0 else 255 / gray_scale)
+
+
 original_img = get_image_array(input("Enter original image file name: "))
 mosaic = create_gray_mosaic(original_img,
-                            int(input("Enter mosaic pixel height (Example: 10): ")),
-                            int(input("Enter mosaic pixel weight (Example: 10): ")),
-                            int(input("Enter gray step (Example: 50): ")))
+                            int(input("Enter mosaic block height (Example: 10): ")),
+                            int(input("Enter mosaic block weight (Example: 10): ")),
+                            get_step(int(input("Enter number of gradations (Example: 10, min: 1, max: 255): "))))
 save_image(mosaic, input("Enter result mosaic file name: "))
