e8799d9c72f654a2658777a6f2484d1248a79b9b
Straordinariu <sasha4983@mail.ru>
2021-11-22 02:19:18 +0500

    этап 3

--- filter.py
+++ filter.py
@@ -2,23 +2,13 @@ import numpy as np
 from PIL import Image
 
 def calc_gray(pix_x, pix_y, size_x, size_y, pix):
-    res = 0
-    for i in range(pix_x, pix_x + size_x):
-        for j in range(pix_y, pix_y + size_y):
-            color_1 = pix[i][j][0]
-            color_2 = pix[i][j][1]
-            color_3 = pix[i][j][2]
-            res += (int(color_1) + int(color_2) + int(color_3)) / 3
+    res = np.sum((pix[pix_x: pix_x + size_x, pix_y: pix_y + size_y]) / 3)
     res = int(res // (size_x * size_y))
     return res
 
 def change_pix(pix_x, size_x, pix_y, size_y, step, pix):
     gray = calc_gray(pix_x , size_x, pix_y, size_y, pix)
-    for i in range(pix_x, pix_x + size_x):
-        for j in range(pix_y, pix_y + size_y):
-            pix[i][j][0] = int(gray // step) * step
-            pix[i][j][1] = int(gray // step) * step
-            pix[i][j][2] = int(gray // step) * step
+    pix[pix_x: pix_x + size_x, pix_y: pix_y + size_y] = int(gray // step) * step
 
 def transform_img(pix, size_x, size_y, grey_step):
     height = len(pix)
