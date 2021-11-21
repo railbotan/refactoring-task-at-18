dc6b0c730f0686c0baa39d3269c7506230ff2a49
Bornovolokova Olga <bornovolokovalelik653@yandex.ru>
2021-11-21 21:04:52 +0500

    Task 1

--- filter.py
+++ filter.py
@@ -1,21 +1,22 @@
 from PIL import Image
 import numpy as np
+
 img = Image.open("img2.jpg")
 arr = np.array(img)
 a = len(arr)
 a1 = len(arr[1])
 i = 0
-while i < a - 11:
+while i < a:
     j = 0
-    while j < a1 - 11:
+    while j < a1:
         s = 0
         for n in range(i, i + 10):
             for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
+                n_1 = arr[n][n1][0]
                 n2 = arr[n][n1][1]
                 n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
-                s += M
+                M = int(n_1) + int(n2) + int(n3)
+                s += M // 3
         s = int(s // 100)
         for n in range(i, i + 10):
             for n1 in range(j, j + 10):
@@ -24,5 +25,6 @@ while i < a - 11:
                 arr[n][n1][2] = int(s // 50) * 50
         j = j + 10
     i = i + 10
+
 res = Image.fromarray(arr)
 res.save('res.jpg')
