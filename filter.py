e38a0fa7152f79aef4da1ca62f41a10845da5aa0
Bornovolokova Olga <bornovolokovalelik653@yandex.ru>
2021-11-21 22:11:46 +0500

    Task 3

--- filter.py
+++ filter.py
@@ -2,23 +2,12 @@ from PIL import Image
 import numpy as np
 
 def get_brightness(arr_pixels, pix_x, pix_y, size_moz):
-    result_grey = 0
-    for row in range(pix_x, pix_x + size_moz):
-            for column in range(pix_y, pix_y + size_moz):
-                first_color = arr_pix[row][column][0]
-                second_color = arr_pix[row][column][1]
-                third_color = arr_pix[row][column][2]
-                sum_color = int(first_color) + int(second_color) + int(third_color)
-                result_grey += int(sum_color // 3 // size_moz ** 2)
-    return result_grey
+    sum_color = np.sum(arr_pixels[pix_x: pix_x + size_moz, pix_y: pix_y + size_moz])
+    return int(sum_color // 3 // size_moz ** 2)
 
 def set_color(arr_pixels, brightness, size_moz, pix_x, pix_y, step):
     value_grey = int(brightness // step) * step
-    for row in range(pix_x, pix_x + size_moz):
-            for column in range(pix_y, pix_y + size_moz):
-                arr_pixels[row][column][0] = value_grey
-                arr_pixels[row][column][1] = value_grey
-                arr_pixels[row][column][2] = value_grey
+    arr_pixels[pix_x: pix_x + size_moz, pix_y: pix_y + size_moz] = value_grey
 
 def grey_img(file_image, gradation, size_moz):
     arr_pixels = np.array(img)
