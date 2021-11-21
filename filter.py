ca114195c92d96a9301c3459f49d756025ca17d0
CyberMagic404 <81950751+CyberMagic404@users.noreply.github.com>
2021-11-21 14:59:13 +0500

    Исправление недочетов.
    1. Удалены лишние циклы.
    2. Исправлены названия переменных.
    3. Улучшено быстродействие программы.

--- filter.py
+++ filter.py
@@ -1,35 +1,25 @@
 from PIL import Image
 import numpy as np
-imgName = input('Введите название исходного изображения (с расширением): ')
-resName = input('Введите название конечного изображения (с расширением): ')
-img = Image.open(imgName)
-imgArray = np.array(img)
-imgArrayLen = len(imgArray)
-innerArrayLen = len(imgArray[1])
-arrCounter = 0
+img_name = input('Введите название исходного изображения (с расширением): ')
+res_name = input('Введите название конечного изображения (с расширением): ')
+img = Image.open(img_name)
+img_array = np.array(img)
 size = int(input('Введите размер мозаики: '))
 step = int(input('Введите количество градаций серого цвета: '))
+grad_step = 255 // step
 
-def CountTotalSum(innerArrCounter, arrCounter):
-    totalSum = 0
-    tempArr = imgArray // 3
-    for ac in range(arrCounter, arrCounter + size):
-        for iac in range(innerArrCounter, innerArrCounter + size):
-            totalSum += sum(tempArr[ac][iac])
-    return int(totalSum // 100)
+def find_av_brightness(part_of_img, size, grad_step):
+    total_sum = np.sum(part_of_img[:size, :size]) / 3
+    av_brightness = total_sum // size * size
+    return int(av_brightness // grad_step) * grad_step
 
-def CreatePixels(innerArrCounter, arrCounter, totalSum):
-    for ac in range(arrCounter, arrCounter + size):
-        for iac in range(innerArrCounter, innerArrCounter + size):
-            imgArray[ac][iac][:] = int(totalSum // step) * step
 
-while arrCounter < imgArrayLen - (size + 1):
-    innerArrCounter = 0
-    while innerArrCounter < innerArrayLen - (size + 1):
-        totalSum = CountTotalSum(innerArrCounter, arrCounter)
-        CreatePixels(innerArrCounter, arrCounter, totalSum)
-        innerArrCounter = innerArrCounter + size - 1
-    arrCounter = arrCounter + size - 1
-res = Image.fromarray(imgArray)
-res.save(resName)
+def convert_to_grey_img(img_array, size, grad_step):
+    for i in range(0, len(img_array), size):
+        for j in range(0, len(img_array[0]), size):
+            img_array[i:i + size, j:j + size] = find_av_brightness(img_array[i:i + size, j:j + size], size, grad_step)
+    return img_array
+
+res = Image.fromarray(convert_to_grey_img(img_array, size, grad_step))
+res.save(res_name)
 print('Преобразование завершено!')
