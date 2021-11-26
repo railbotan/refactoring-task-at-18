cdfff7fb6fb9f5529a56e901bc003d22d49bd94e
Pasha0909 <Samoylov0909@mail.ru>
2021-11-26 17:32:05 +0500

    task_3

--- filter.py
+++ filter.py
@@ -3,23 +3,12 @@ import numpy as np
 img = Image.open("img2.jpg")
 
 def get_illumination(pixels, p_x, p_y, size):
-    result = 0
-    for range1 in range(p_x, p_x + size):
-        for pillar in range(p_y, p_y + size):
-            color_1 = int(pix[pillar][range1][0])
-            color_2 = int(pix[pillar][range1][1])
-            color_3 = int(pix[pillar][range1][2])
-            summa = color_1 + color_2 + color_3
-            result += int(summa // 3)
-    result = int(result // (size * size))
-    return result
+    summa = np.sum(pixels[p_x: p_x + size, p_y: p_y + size])
+    result = int(summa // 3)
+    return int(result // (size * size))
 
 def install_color(pixels, illumination, size, p_x, p_y, step):
-    for range1 in range(p_x, p_x + size):
-        for pillar in range(p_y, p_y + size):
-            pixels[range1][pillar][0] = int(illumination // step) * step
-            pixels[range1][pillar][1] = int(illumination // step) * step
-            pixels[range1][pillar][2] = int(illumination // step) * step
+    pixels[p_x: p_x + size, p_y: p_y + size] = int(illumination // step) * step
 
 def get_gray(file_image, gradation, size):
     step = 255 // graduation
