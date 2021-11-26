d1dbab1298c359c21855f6a9d7ea2eb3784a5d81
Pasha0909 <Samoylov0909@mail.ru>
2021-11-26 17:26:56 +0500

    task_2

--- filter.py
+++ filter.py
@@ -1,28 +1,36 @@
 from PIL import Image
 import numpy as np
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a:
-    j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                a1 = int(arr[n][n1][0])
-                a2 = int(arr[n][n1][1])
-                a3 = int(arr[n][n1][2])
-                M = a1 + a2 + a3
-                s += M // 3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
+
+def get_illumination(pixels, p_x, p_y, size):
+    result = 0
+    for range1 in range(p_x, p_x + size):
+        for pillar in range(p_y, p_y + size):
+            color_1 = int(pix[pillar][range1][0])
+            color_2 = int(pix[pillar][range1][1])
+            color_3 = int(pix[pillar][range1][2])
+            summa = color_1 + color_2 + color_3
+            result += int(summa // 3)
+    result = int(result // (size * size))
+    return result
+
+def install_color(pixels, illumination, size, p_x, p_y, step):
+    for range1 in range(p_x, p_x + size):
+        for pillar in range(p_y, p_y + size):
+            pixels[range1][pillar][0] = int(illumination // step) * step
+            pixels[range1][pillar][1] = int(illumination // step) * step
+            pixels[range1][pillar][2] = int(illumination // step) * step
+
+def get_gray(file_image, gradation, size):
+    step = 255 // graduation
+    pixels = np.array(img)
+    hei = len(pixels)
+    wid = len(pixels[1])
+    for y in range(0, hei, size):
+        for x in range(0, wid, size):
+            illumination = get_illumination(pixels, p_x, p_y, size)
+            install_color(pixels, illumination, size, p_x, p_y, step)
+    return pixels
+
+res = Image.fromarray(grey_img(img, 10, 50))
 res.save('res.jpg')
