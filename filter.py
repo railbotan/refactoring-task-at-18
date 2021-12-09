9813a9e3c752126d79620cdbeb74e02f28e5f600
Pasha0909 <Samoylov0909@mail.ru>
2021-12-09 19:40:07 +0500

    task 4 refactoring

--- filter.py
+++ filter.py
@@ -9,9 +9,8 @@ def get_illumination(pixels, p_x, p_y, size):
 def install_color(pixels, illumination, size, p_x, p_y, step):
     pixels[p_x: p_x + size, p_y: p_y + size] = int(illumination // step) * step
 
-def get_gray(img, gradation, size):
-    pixels = np.array(Image.open(img)).astype(int)
-    step = 255 // graduation
+def get_gray(pixels, gradation, size):
+    step = 255 // (gradation - 1)
     hei = len(pixels)
     wid = len(pixels[1])
     for y in range(0, hei, size):
@@ -20,7 +19,9 @@ def get_gray(img, gradation, size):
             install_color(pixels, illumination, size, x, y, step)
     return pixels
 
-res = Image.fromarray(get_gray(input("Введите название входного файла"),
+img = Image.open('img2.jpg')
+pixels = np.array(img)
+res = Image.fromarray(get_gray(pixels,
                                 int(input("Введите размер градации")), 
                                 int(input("Введите размер мозаики"))))
-res.save('res.jpg')
+res.save('res1.jpg')
