08d3cdc9e7a6a2866e44edef601dbd07e5e19d5f
KrawczowaKris <nukinanada@gmail.com>
2021-12-13 16:14:36 +0500

    Этап 3

--- filter.py
+++ filter.py
@@ -2,20 +2,11 @@ from PIL import Image
 import numpy as np
 
 def get_grey(s0, i, j):
-    for n in range(i, i + mosaic_size):
-        for k in range(j, j + mosaic_size):
-            size[n][k][0] = int(s0 // grayscale) * grayscale
-            size[n][k][1] = int(s0 // grayscale) * grayscale
-            size[n][k][2] = int(s0 // grayscale) * grayscale
+    size[i: i + mosaic_size, j: j + mosaic_size, 0] = int(s0 // grayscale) * grayscale
+    size[i: i + mosaic_size, j: j + mosaic_size, 1] = int(s0 // grayscale) * grayscale
+    size[i: i + mosaic_size, j: j + mosaic_size, 2] = int(s0 // grayscale) * grayscale
 def get_px(i, j):
-    s0 = 0
-    for n in range(i, i + mosaic_size):
-        for k in range(j, j + mosaic_size):
-            a = int(size[n][k][0])
-            b = int(size[n][k][1])
-            c = int(size[n][k][2])
-            s0 += (a + b + c) // 3
-    return s0
+    return np.sum(size[i: i + mosaic_size, j: j + mosaic_size]) // 3
 
 grayscale = int(input())
 mosaic_size = int(input())
