6c41dddc2ff2be177ad4e6f32163b5887b18caa3
KrawczowaKris <nukinanada@gmail.com>
2021-12-13 16:11:40 +0500

    Этап 2

--- filter.py
+++ filter.py
@@ -1,29 +1,39 @@
 from PIL import Image
 import numpy as np
 
+def get_grey(s0, i, j):
+    for n in range(i, i + mosaic_size):
+        for k in range(j, j + mosaic_size):
+            size[n][k][0] = int(s0 // grayscale) * grayscale
+            size[n][k][1] = int(s0 // grayscale) * grayscale
+            size[n][k][2] = int(s0 // grayscale) * grayscale
+def get_px(i, j):
+    s0 = 0
+    for n in range(i, i + mosaic_size):
+        for k in range(j, j + mosaic_size):
+            a = int(size[n][k][0])
+            b = int(size[n][k][1])
+            c = int(size[n][k][2])
+            s0 += (a + b + c) // 3
+    return s0
+
+grayscale = int(input())
+mosaic_size = int(input())
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
+
+size = np.array(img)
+height = len(size)
+width = len(size[1])
+
 i = 0
-while i < a:
+
+while i < height:
     j = 0
-    while j < a1:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                a = int(arr[n][n1][0])
-                b = int(arr[n][n1][1])
-                c = int(arr[n][n1][2])
-                M = a + b + c
-                s += M // 3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
+    while j < width:
+        s = int(get_px(i, j) // (mosaic_size * mosaic_size))
+        get_grey(s, i, j)
+        j = j + mosaic_size
+    i = i + mosaic_size
+res = Image.fromarray(size)
 res.save('res.jpg')
